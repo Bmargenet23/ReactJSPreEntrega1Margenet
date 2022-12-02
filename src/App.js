2b81900023fa7d0cc446ab69fd3605d1bf9a1132
Brian Margenet <brianmargenet@gmail.com>
2022-12-01 23:56:59 -0300

    Primer Repo ReactJS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar/>
+      <ItemListContainer texto="Camisetas Fanatics"/>
+      <header className="App-header bg-secondary">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
