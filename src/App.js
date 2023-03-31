6e15988a903b4b3dbb1d4e343b31894a92d01cff
Brian Margenet <brianmargenet@gmail.com>
2023-03-30 23:23:17 -0300

    primer entrega reactjs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import ItemListContainer from './components/ItemListContainer';
+import Navbar from './components/NavBar/Navbar';
+import ItemListContainer from './components/ItemListContanier/ItemListContainer';
+import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <ItemListContainer texto="Camisetas Fanatics"/>
-      <header className="App-header bg-secondary">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <ItemListContainer texto="Camisetas Fanatics"/>
+        <header className="App-header bg-secondary">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Routes>
+        
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,32 +1,33 @@
 import React from 'react'; 
-import Cartwidget from './Cartwidget';
+import Cartwidget from '../CartWidget/Cartwidget';
 
 const navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Camisetas Fanatics</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Nosotros</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Productos
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Selecciones</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+            <li><a className="dropdown-item" href="#">Equipos de Argentina</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Resto del mundo</a></li>
+          </ul>   
         </li>
         <li className="nav-item">
           <a className="nav-link disabled">Disabled</a>
